88b393ec1b3837fe5aecc10217c6cd3c04d005cd
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-11-09 02:51:58 -0800

    Update buildscript.sh

--- buildscript.sh
+++ buildscript.sh
@@ -48,7 +48,7 @@ popd
 # git submodule update --recursive
 git status && git add -A && git status
 git commit -a -S -m "Successful Build of Release 2024-10-30" && git push --set-upstream origin builder
-git tag -a 2024-10-30 -S -m "First tagged release" && git push origin 2024-10-30
+git tag -a 2024-10-30 -s -m "First tagged release" && git push origin 2024-10-30
 docker logout
 
 snap disable docker
