3974e4639e1542a2dd4282217f5ab6b2302204c2
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2026-01-16 19:07:24 -0800

    Update buildscript.sh

--- buildscript.sh
+++ buildscript.sh
@@ -45,10 +45,10 @@ chown root:root /var/snap/docker
 snap install docker --revision=3380
 
 {
-git config --global --add safe.directory $HOME/Debian
-git config --global --add safe.directory $HOME/Debian/debian-slim
-git config --global --add safe.directory $HOME/Debian/debian
-git config --global --add safe.directory $HOME/Debian/debian-extra
+git config --global --add safe.directory $(echo $HOME)/Debian
+git config --global --add safe.directory $(echo $HOME)/Debian/debian-slim
+git config --global --add safe.directory $(echo $HOME)/Debian/debian
+git config --global --add safe.directory $(echo $HOME)/Debian/debian-extra
 git remote remove origin && git remote add origin git@Debian:0mniteck/Debian.git
 git submodule update --init --remote --merge
 docker buildx create --name debian-builder --driver-opt "network=host" --bootstrap --use
