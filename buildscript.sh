e15d1c17d7d6c7f7d10151a25b94ddf05dfeae39
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2026-01-28 16:46:48 -0800

    Update buildscript.sh

--- buildscript.sh
+++ buildscript.sh
@@ -5,8 +5,8 @@ run_as=$(id -u $run_id -n)
 home=/home/$run_as
 data_dir=$home/.local/share
 sed_ech=$(cat << _EOF__
-\[Service\]
-Group=$run_as
+\[Service\]\
+Group=$run_as\
 Slice=docker.slice
 _EOF__
 )
