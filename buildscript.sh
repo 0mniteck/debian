371d1389f8d6c82837a202a48263e2aa7a3a8434
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2026-01-29 22:46:32 -0800

    Update buildscript.sh

--- buildscript.sh
+++ buildscript.sh
@@ -97,7 +97,7 @@ BUILDX_METADATA_WARNINGS=1
 PATH=/usr/sbin:/usr/bin:/snap/bin:$docker_path\" >> $rootless_path/env-rootless
 \$(echo \"echo \$\(\<$rootless_path/env-rootless\)\" $(echo $docker)d --rootless \
 --userland-proxy-path=$docker_path/docker-proxy --init-path=$docker_path/docker-init \
---feature cdi=false --group docker | /bin/bash 2>> $rootless_path/rootless.log'
+--feature cdi=false --group docker\") | /bin/bash 2>> $rootless_path/rootless.log'
 __EOF
 chmod +x $rootless_path.sh
 
