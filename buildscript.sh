d2c642c8a2c6e8cbe29b52f2e9e569be4d704725
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2026-01-30 14:52:57 -0800

    Update buildscript.sh

--- buildscript.sh
+++ buildscript.sh
@@ -3,11 +3,8 @@
 rel_date="01-29-2026"
 date_rel="2026-01-29"
 docker_ver=3380
-debug() {
-eval $(set -x)
-}
 
-debug="debug()" # uncomment to enable debugging
+debug="eval $(set -x)" # uncomment to enable debugging
 
 debian_security=20260125T223411Z
 debian=20260125T203410Z
