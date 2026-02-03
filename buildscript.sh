46399847631ebfa74495a938a1b920dc2636ace0
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2026-02-03 05:49:36 -0800

    Update buildscript.sh

--- buildscript.sh
+++ buildscript.sh
@@ -203,8 +203,12 @@ fi
 eval \"\$(ssh-agent -s)\"
 ssh-add -l -L -t 1D -h git@github.com $home/.ssh/id_ecdsa_s*[!.pub]
 systemctl --user restart gpg-agent* --all && wait
-git remote remove origin && git remote add origin git@Debian:0mniteck/Debian.git
 git submodule update --init --remote --merge
+
+git remote remove origin && git remote add origin git@Debian:0mniteck/Debian.git
+git config --global user.email 10482171+0mniteck@users.noreply.github.com
+git config --global user.name "Shant Patrick Tchatalbachian"
+git config --global user.signingkey 287EE837E6ED2DD3
 
 if [[ \"\$(gpg-card list)\" == *42E2DDF1E31B370F8BFFEE03287EE837E6ED2DD3* ]]; then
   echo && echo \"Signing key 287EE837E6ED2DD3 present\" && echo
