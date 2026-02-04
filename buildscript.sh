ce5517f4dd32175cff00e35f584414c07f10229e
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2026-02-03 18:09:16 -0800

    Update buildscript.sh

--- buildscript.sh
+++ buildscript.sh
@@ -187,8 +187,9 @@ scan_using_grype() { # $1 = Name, $2 = Name:tag
 systemctl --user reset-failed && wait
 systemctl --user stop docker* --all && wait
 systemctl --user list-units docker* --all
+systemctl --user daemon-reload
 systemctl --user start docker.dockerd && sleep 10
-systemctl --user status docker* --all --no-pager -n 150 > $rootless_path/rootless.ctl.log
+systemctl --user status docker.dockerd --all --no-pager -n 150 > $rootless_path/rootless.ctl.log
 
 source $rootless_path/env-rootless.exp
 $docker info | grep \"rootless\" > $rootless_path/rootless.status
