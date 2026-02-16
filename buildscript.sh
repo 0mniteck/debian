9121bedf0d8627d61ba97ecbcfeac89c5d738ee8
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2026-02-15 18:40:15 -0800

    Update buildscript.sh

--- buildscript.sh
+++ buildscript.sh
@@ -116,6 +116,7 @@ HOME=$home
 
 ssh_conf=\$(<$home/.ssh/config) || mkdir -p $home/.ssh && touch $home/.ssh/config && \
 chmod 0644 $home/.ssh/config && chmod 0700 $home/.ssh
+
 systemctl --user restart gpg-agent.service && wait
 export GPG_TTY=\$(tty)
 
@@ -303,6 +304,7 @@ snap remove syft --purge
 
 sed -i "s|:/home/root:|:/root:|" /etc/passwd
 delgroup docker
+
 clean_all
 systemctl daemon-reload
 exit 0
