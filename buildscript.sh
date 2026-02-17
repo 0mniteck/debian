a21312360303c14215bb807c0107806fff663b54
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2026-02-16 22:56:32 -0800

    Update buildscript.sh

--- buildscript.sh
+++ buildscript.sh
@@ -200,18 +200,6 @@ sed -z -i \"s|\[Service\]\nEnv|$(printf \"%s\\\\n\" $(echo $sed_ech))Env|\" $sys
 sed -i \"s|EnvironmentFile.*|EnvironmentFile=-$rootless_path/env-rootless|\" $sysusr_service
 sed -i \"s|ExecStart.*|ExecStart=/bin/bash -c \'$data_dir/rootless.sh\'|\" $sysusr_service
 
-functitup() {
-tcols=\$(tput cols); size=\$tcols; i=0
-spin='�￹ºk�￹7l�￹û²�￹�￹bn�￹á�￹�￹&¾ì £￹�￹¦¸cv�￹¯»((�￹&é®￹�￹²kî�￹1�￹ºk�￹7l�￹û²�￹�￹bn�￹á�￹�￹&¾ì £￹�￹¦¸cv�￹¯»((�￹&é®￹�￹²kî�￹1�￹ºk�￹7l�￹û²�￹�￹bn�￹á�￹�￹&¾ì £￹�￹¦¸cv�￹¯»((�￹&é®￹�￹²kî�￹1�￹ºk�￹7l�￹û²�￹�￹bn�￹á�￹�￹&¾ì £￹�￹¦¸cv'
-len=208
-while :; do
-  i=\$(( (i+size) % len))
-  printf '%s\r'  \"\${spin:\$i:\$size}\"
-  sleep .05
-  size=\$(shuf -i1-\$tcols -n1 --random-source=/dev/random)
-done
-}
-
 mkdir -p $docker_data/syft && mkdir -p $docker_data/grype
 scan_using_grype() { # $1 = Name, $2 = Repo/Name:tag or /Path --select-catalogers debian, $3 = Attest Tag
   grype config > $docker_data/.grype.yaml
@@ -227,11 +215,9 @@ scan_using_grype() { # $1 = Name, $2 = Repo/Name:tag or /Path --select-cataloger
   TMPDIR=$docker_data/syft syft scan \$2 -o spdx-json=\$1.spdx.json || exit 1
   rm -f -r $docker_data/syft/*
   echo && echo 'Starting Grype...'
-  functitup & echo \$! > .pid && pid=\$(<\.pid) && rm -f .pid
   script -q -c \"TMPDIR=$docker_data/grype grype sbom:\$1.spdx.json \
   -c $docker_data/.grype.yaml -o json > \$1.grype.json\" \$1.grype.tmp.tmp > \$1.grype.tmp
   rm -f -r $docker_data/grype/*
-  trap '[[ \$pid ]] && kill \$pid; exit' EXIT && echo
   marker() { # $1 = Name, $2 = Order, $3 = Marker/ID
     unset \"wright\$2\"
     grep \"\$3\" \$1.grype.tmp | tail -n 1 > \$1.grype.status.\$2
